be3163358b2aa01dab96fb67c95b4c74fcd43ab9
Chris <jiulrulsmu@gmail.com>
2022-12-11 22:11:35 +0800

    Beta10

--- app.py
+++ app.py
@@ -11,6 +11,16 @@ from line_notify import lineNotifyMessage
 from roboflow import Roboflow
 
 
+
+Bee_rf = Roboflow(api_key=os.getenv('api_key'))
+Bee_project = Bee_rf.workspace().project("honey-bee-detection-model-zgjnb")
+Bee_model = Bee_project.version(2).model
+
+Hornet_rf = Roboflow(api_key=os.getenv('api_key'))
+Hornet_project = Hornet_rf.workspace().project("bee-d4yoh")
+Hornet_model = Hornet_project.version(5).model
+
+
 # create the extension
 db = SQLAlchemy()
 
@@ -90,6 +100,28 @@ def Reocrd_serializer(Reocrd):
         'CreateTime' : Reocrd.CreateTime
     }
 
+@app.route('/ReactUpload', methods=['POST'])
+def fileUpload():
+    file = request.files['file'] 
+    filename = secure_filename(file.filename)
+    file.save(app.config["UPLOADED_PHOTOS_DEST"]+"/"+filename)
+    return dectectAndNotify("uploads/"+filename)
+
+def dectectAndNotify(path):
+    beens = Bee_model.predict(path, confidence=40, overlap=30).json()
+    numberOfBees =  len([x for x in beens['predictions'] if x['class'] == 'bee'])
+    hiveID = random.randint(1,5)
+    hornets = Hornet_model.predict(path, confidence=40, overlap=30).json()
+    hasHornets = 'Y' if len([x for x in hornets['predictions'] if x['class'] == 'Asian Hornet']) > 0 else 'N'
+    res = ""
+    if numberOfBees>0:
+        res = AddData(hiveID,numberOfBees,hasHornets)
+    if hasHornets == 'Y':
+        if not os.path.exists(app.config["PREDICT_PHOTOS_DEST"]):
+            os.mkdir(app.config["PREDICT_PHOTOS_DEST"])
+        Hornet_model.predict(path, confidence=40, overlap=30).save(app.config["PREDICT_PHOTOS_DEST"] +"/prediction.jpg")
+        lineNotifyMessage('注意!!疑似虎頭蜂出沒',"predict/prediction.jpg",app.config["LINE_TOKEN"] )
+    return res
 
 def AddData(hiveID,numberOfBees,hasHornets):
     try:
